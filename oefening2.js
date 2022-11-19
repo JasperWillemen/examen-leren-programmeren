15d65b183af2a45a5615ccf363d4c68d59cafb0f
JasperWillemen <willemen.j@gmail.com>
2022-11-19 12:11:32 +0100

    Valt het op dat ik hier maar wat aan 't lullen ben bij gebrek aan nuttige updates?

--- oefening2.js
+++ oefening2.js
@@ -1,13 +1,12 @@
 function Text(string) {
     this.str = string;
-
 }
 
 Text.prototype.left = function(char) {
-    const leftString = this.str.substring(0, char)
+    const leftString = this.str.slice(0, char)
     return leftString
 }
 
 let string1 = new Text('testing this bullshit');
 
-Text.left(2);
+console.log(string1.left(7))
